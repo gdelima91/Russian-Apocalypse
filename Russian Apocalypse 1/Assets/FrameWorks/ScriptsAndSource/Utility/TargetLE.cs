8238c9a71ba8e45170de0d3e54205711a01f8030
DELIMALAPTOP\Gavin Delima <gdelima91@gmail.com>
2017-10-26 14:09:21 -0700

    Added mixamo mesh, anims, edited bullet code, added enemy script, enemy bullet code

--- Russian Apocalypse 1/Assets/FrameWorks/ScriptsAndSource/Utility/TargetLE.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//This Represent the Target Live Entity which we currently target on.
-//which means the character we currently controlled. the Main UI target on.......
-public class TargetLE : MonoBehaviour {
-    
-}
