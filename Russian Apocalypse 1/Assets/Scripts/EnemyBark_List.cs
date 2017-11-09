eb00f0fccb4ba58dd12a3c35a75dcc5751aba5d1
DELIMALAPTOP\Gavin Delima <gdelima91@gmail.com>
2017-11-09 13:06:54 -0800

    added swat model, guncontroller script

---
+++ Russian Apocalypse 1/Assets/Scripts/EnemyBark_List.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBark_List : MonoBehaviour {
+
+    public List<AudioClip[]> clips = new List<AudioClip[]>();
+
+    public enum FolderName : int {
+        onHit   = 1,
+        onDeath = 2
+    }
+
+    // Use this for initialization
+    void Start () {
+        clips.Add(Resources.LoadAll<AudioClip>("Sounds/Quotes/onHit"));
+        clips.Add(Resources.LoadAll<AudioClip>("Sounds/Quotes/onDeath"));
+    }
+}
