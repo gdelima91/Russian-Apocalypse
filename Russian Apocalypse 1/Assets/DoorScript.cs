be488a1ffb2a98a7fdd721703ebbaf767ef896e7
DESKTOP-54DNJUK\Gavin <gdelima91>
2017-12-04 21:59:18 -0800

    Added HP UI, ragdoll prefab made, not implemented, enemy spawner prefab made, door open script added.

---
+++ Russian Apocalypse 1/Assets/DoorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    public bool opensFromFront = false;
+    public bool opensFromBack = true;
+    private Animator animator;
+
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+    public void OpenDoor (Triggers trigger) {
+        if (trigger == Triggers.Front && opensFromFront) {
+            animator.SetBool("Open", true);
+        }
+
+        if (trigger == Triggers.Back && opensFromBack) {
+            animator.SetBool("Open", true);
+        }
+    }
+
+    public enum Triggers {Front, Back};
+}
---
+++ Russian Apocalypse 1/Assets/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyToSpawn;
+    public float timeBetweenSpawns;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        SpawnEnemy();
+        GetComponent<MeshRenderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnEnemy () {
+        GameObject enemy1 = Instantiate(enemyToSpawn, transform.position, transform.rotation) as GameObject;
+        enemy1.GetComponent<Enemy1_StateMachine>().currentTFTarget = player.transform;
+        StartCoroutine("SpawnTimer");
+    }
+
+    IEnumerator SpawnTimer () {
+        yield return new WaitForSeconds(timeBetweenSpawns);
+        SpawnEnemy();
+    }
+}
