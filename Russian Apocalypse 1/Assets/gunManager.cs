b8687213af710c216ebe349fb35444a1ab929883
DELIMALAPTOP\Gavin Delima <gdelima91@gmail.com>
2017-12-07 15:08:33 -0800

    see below

---
+++ Russian Apocalypse 1/Assets/gunManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunManager : MonoBehaviour {
+
+    public List<MonoBehaviour> monos;
+    public int ind = 0;
+
+    void Start() {
+        Equp(0);
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(1)) {
+            Next();
+        }
+	}
+
+    void Equp(int index) {
+       
+        monos[index].gameObject.SetActive(true);
+    }
+
+    void Next() {
+        
+        monos[ind].gameObject.SetActive(false);
+        ind++;
+        ind %= monos.Count;
+        monos[ind].gameObject.SetActive(true);
+    }
+
+    public void AddGun(MonoBehaviour g) {
+        g.gameObject.SetActive(false);
+        monos.Add(g);
+    }
+}
