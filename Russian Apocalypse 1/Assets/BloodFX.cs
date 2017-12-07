e1c9a06923e3f0980e76fb8008435b50d9e05553
DESKTOP-54DNJUK\Gavin <gdelima91>
2017-12-06 16:36:28 -0800

    edited gun script to no longer minus bullets in mag count, enemy now spawns ragdoll on death, enemy spawns blood on death, but buggy, shotgun script created, bullets do not go towards mouse.

---
+++ Russian Apocalypse 1/Assets/BloodFX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodFX : MonoBehaviour {
+
+    public GameObject blood;
+    private CapsuleCollider capsuleCollider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        
+        if (collision.gameObject.layer == 10) {
+            print("Hit");
+            Instantiate(blood, transform.position, transform.rotation);
+        }
+    }
+}
